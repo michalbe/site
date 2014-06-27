c1c4a432d5b17d99f17356fc38c515a6047879bc
Michal Budzynski <michal@virtualdesign.pl>
2014-06-27 02:25:25 +0200

    [SITE] - :persevere: Shuffle speakers

--- js/script.js
+++ js/script.js
@@ -9,4 +9,14 @@ window.onload = function() {
   for (var i=0, l=buttons.length; i<l; i++) {
     buttons[i].addEventListener('click', listener);
   }
+
+  // shuffle speakers
+  var speakers = document.querySelectorAll('#speakers-page img');
+
+  for (i=0, l=speakers.length; i<l; i++) {
+    speakers[i].style.transform =
+      "translateX(" + ~~(30 - Math.random()*60) +
+      "px) translateY(" + ~~(Math.random()*300) + "px)";
+  }
+
 }
